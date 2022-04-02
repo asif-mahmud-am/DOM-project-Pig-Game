7947cd1fe1c30de39ea1e8aa44e0a5b988481dfe
Asif Mahmud <65419625+asif-mahmud-am@users.noreply.github.com>
2022-04-02 19:31:50 +0600

    Add files via upload

---
+++ starter/test.js
@@ -0,0 +1,27 @@
+//var x = 6;
+//var y = 6;
+//
+//if(x ==6 && y == 6)
+//    {
+//        console.log('yes');
+//    }
+//
+//console.log('Yesss');
+
+var hScore = 30;
+
+
+
+ 
+click(h)
+{
+   var h; 
+   if(h)
+       {
+           
+       }
+    
+    
+    
+}
+
